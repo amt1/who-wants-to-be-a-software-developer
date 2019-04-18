06f96e69aa6189a4bc5d7dd97af85b99b337965c
lidek <lidia.stopinska@gmail.com>
2019-04-18 17:31:27 +0100

    updated db structure

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,9 +2,15 @@ use pda_hub;
 db.dropDatabase();
 
 db.pda.insertMany([
-  {
+  {"category":"",
+  "type":"boolean",
+  "difficulty":"medium",
+  "question":"",
+  "correct_answer":"True",
+  "incorrect_answers":"False"}
+
 
   },
-  
+
 
 ]);
