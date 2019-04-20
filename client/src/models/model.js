ad85dff08bbbd289353435620a9e3f6c7dbcb0b4
Annabel Treshansky <atreshansky@googlemail.com>
2019-04-20 23:48:14 +0100

    trying to run testing with mochs but getting error No test files found

--- client/src/models/model.js
+++ client/src/models/model.js
@@ -1,11 +1,13 @@
 const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
-const QuizModel = function () {
-  this.url = 'http://localhost:3000/api/pda_quiz';
-  this.request = new RequestHelper(this.url);
-  this.numberOfQuestions = 0;
-  this.difficulty = "";
+const QuizModel = function (level, numberOfQuestions) {
+  this.quizUrl = 'http://localhost:3000/api/pda_questions';
+  this.quizRequest = new RequestHelper(this.quizUrl);
+  this.numberOfQuestions = numberOfQuestions;
+  this.levels = ["easy","medium","hard"];
+  this.level = level % 3;
+  this.difficulty = this.levels[this.level];
   this.runningTotal = 0;
   this.questions = [];
 };
@@ -20,12 +22,23 @@ QuizModel.prototype.bindEvents = function () {
   // })
 };
 
-QuizModel.prototype.getQuestions = function (this.numberOfQuestions, this.difficulty) {
+QuizModel.prototype.getQuestions = function (numberOfQuestions, difficulty) {
   // this.request.get()
   //   .then((bucketList) => {
   //     PubSub.publish('BucketList:data-loaded', bucketList);
   //   })
   //   .catch(console.error);
+};
+
+QuizModel.scrollLevel = function (level) {
+    return (level++) % 3;
+};
+QuizModel.levelUp = function (level) {
+    if (level) return 2;
+    return 1;
+};
+QuizModel.levelDown = function (level) {
+    return (level--) % 3;
 };
 
 QuizModel.prototype.quizLoop = function () {
