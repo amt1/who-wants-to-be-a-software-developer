a201c9e8125ff5264668b9cc27b6b11aa6dbbc5a
lidek <lidia.stopinska@gmail.com>
2019-04-21 15:08:04 +0100

    updated chanels

--- client/src/views/quiz_select_view.js
+++ client/src/views/quiz_select_view.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-// const PubSub = require('../helpers/pub_sub.js');
-//
-// const QuizSelectView = function(element) {
-//   this.element = element;
-// };
-//
-// QuizSelectView.prototype.bindEvents = function () {
-//   PubSub.subscribe('Quiz:quizes-ready', (evt) => {
-//     this.populateQuiz(evt.detail);
-//   });
-//    this.selectElement.addEventListener('submit', (evt) => {
-//     const quiz = evt.target;
-//     PubSub.publish('QuizSelectView:quiz-selected', quiz);
-//   });
-// };
-//
-// QuizSelectView.prototype.populateQuiz = function (quizes) {
-//   quizes.forEach((question, index) => {
-//     const option = this.createQuizOption(question, index);
-//     this.selectElement.appendChild(option);
-//   })
-// };
-//
-// QuizSelectView.prototype.createQuizOption = function (question, index) {
-//   const option = document.createElement('option');
-//   option.textContent = quiz;
-//   option.value = index;
-//   return option;
-//   console.log(option)
-// };
-// module.exports = QuizSelectView;
-=======
 const PubSub = require('../helpers/pub_sub.js');
 
 const QuizSelectView = function(element) {
@@ -38,7 +5,7 @@ const QuizSelectView = function(element) {
 };
 
 QuizSelectView.prototype.bindEvents = function () {
-  PubSub.subscribe('Quiz:quizes-ready', (evt) => {
+  PubSub.subscribe('Quiz:question-ready', (evt) => {
     this.populateQuiz(evt.detail);
   });
    this.selectElement.addEventListener('submit', (evt) => {
@@ -62,4 +29,3 @@ QuizSelectView.prototype.createQuizOption = function (question, index) {
   console.log(option)
 };
 module.exports = QuizSelectView;
->>>>>>> origin/feature/quizselectview
