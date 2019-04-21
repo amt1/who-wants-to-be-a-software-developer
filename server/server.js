d7c1f1f7d30e0ec03d74c5b9a1c30c28ac20fc34
Annabel Treshansky <atreshansky@googlemail.com>
2019-04-21 02:03:36 +0100

    Trying to read from database. Route works in Insomnia but I'm getting ReferenceError: fetch is not defined

--- server/server.js
+++ server/server.js
@@ -1,13 +1,3 @@
-// const express = require('express');
-// const app = express();
-//
-// app.use(express.static('client/public'));
-//
-// app.listen(3000, function () {
-//   console.log(`App running on port ${ this.address().port }`);
-// });
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
