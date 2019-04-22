687d08f8c59886d30a2b168ae8f4b1466cc809c1
Katharina Bitzan <katharinabitzan@gmail.com>
2019-04-22 09:53:39 +0100

    user

--- client/src/models/user.js
+++ client/src/models/user.js
@@ -1,6 +1,6 @@
 const PubSub = require('../helpers/pub_sub.js')
 const RequestHelper = require('../helpers/request_helper.js')
-const Quiz = require('./model.js')
+const Quiz = require('./quiz.js')
 
 const User = function () {
 
