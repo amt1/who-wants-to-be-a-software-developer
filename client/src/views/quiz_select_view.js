eff6f82fbd225f1e0771f43c0547e4f0d40dbd92
Katharina Bitzan <katharinabitzan@gmail.com>
2019-04-23 09:25:41 +0100

    edit quiz select view

--- client/src/views/quiz_select_view.js
+++ client/src/views/quiz_select_view.js
@@ -6,10 +6,9 @@ const QuizSelectView = function(element) {
 
 QuizSelectView.prototype.bindEvents = function () {
    this.selectElement.addEventListener('click', (evt) => {
-    const quiz = evt.target;
+    const category = evt.target.id;
 
-    // console.log(evt.target.id)
-    PubSub.publish('QuizSelectView:quiz-selected', quiz);
+    PubSub.publish('QuizSelectView:quiz-selected', category);
   });
 };
 
