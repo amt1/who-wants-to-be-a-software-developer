e0725eeccf7f96a3c038eaf4901aaaee52e3d3e1
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-04-23 23:05:59 +0100

    recursive version of the quiz loop so nearly works

--- client/src/models/quiz_looper.js
+++ client/src/models/quiz_looper.js
@@ -1,9 +1,7 @@
-// const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
 const QuizLooper = function () {
   this.questionsArray = [];
-  this.loopCounter = 0;
   this.runningTotal = 0;
   this.answerResult = [];
   this.currentCategory = 0;
@@ -11,51 +9,27 @@ const QuizLooper = function () {
 
 QuizLooper.prototype.bindEvents = function () {
   PubSub.subscribe('QuestionFetcher:questions-by-category-ready', (allSelectedQuestions) => {
-    // still this bit left to test
   this.questionsArray = allSelectedQuestions.detail[0];
   this.currentCategory = allSelectedQuestions.detail[1];
-  this.loopCounter = this.questionsArray.length;
-console.log("loopCounter: number of questions is:", this.loopCounter);
-  // using a for loop in case we need the counter for any extensions later
-  // no - for loop won't complete in sequence with asynchronous processes inside it
-  // use forEach instead
-//  for (let questionNumber = 0; questionNumber < this.loopCounter; questionNumber++) {
-this.questionsArray.forEach(this.questionLoop);
-  //     console.log('current question: ', currentQuestion);
-  //     if (waitingForAnswer == false) {
-  // //    PubSub.publish('QuizLooper:question-ready', this.questionsArray[questionNumber])
-  // PubSub.publish('QuizLooper:question-ready', currentQuestion)
-  //     waitingForAnswer = true;
-  //   //  while (waitingForAnswer) { // logic works but crashes / hangs browser
-  //         console.log('waiting for answer: ', waitingForAnswer);
-  //
-  //         PubSub.subscribe('QuizView:answer-selected', (questionAndAnswer) => {
-  //           console.log('got answer to check: ', questionAndAnswer.detail);
-  //           const answer = questionAndAnswer.detail[0];
-  //           const question = questionAndAnswer.detail[1];
-  //           this.answerResult = this.checkAnswer(question, answer);
-  //           if (this.answerResult[0] == false) {
-  //             this.addScoreToTotal(this.answerResult[1]);
-  //           }; // end if
-  //           PubSub.publish('QuizLooper:answer-checked', this.answerResult);
-  //           waitingForAnswer = false;
-  //           console.log('waiting for answer: ', waitingForAnswer);
-  //   // this waitingForAnswer loop seems to be working but I'm not convinced it should!
-  //   // ha - it 'worked' because the category only had 1 question in it! Back to the while loop
-  //   // while loop won't work as it ties up the program and prevents the user actually getting the question
-  //           }); // end subscribe
-  //   //      }; // end while
-  //
-  //       }; // end if
-  //   //  }; // end for
-
-// }); // end forEach
+  let numberOfQuestionsAnswered = 0;
+  numberOfQuestionsAnswered = this.quizLoop( this.questionsArray );
+//  console.log('all done, number answered: ', numberOfQuestionsAnswered);
+// this doesn't happen in sequence - it gets to this line first time round with the variable undefined
+// even though I've initaialised it
   }); // end subscribe
 }; // end bindEvents
+
+QuizLooper.prototype.quizLoop = function (questionsArray) {
+  const loopCounter = 0;
+  let currentQuestion = questionsArray[0];
+  return this.questionLoop(currentQuestion, loopCounter, questionsArray);
+};
 
 QuizLooper.prototype.questionLoop = function (currentQuestion, loopCounter, questionsArray) {
   console.log("hello I'm in the question loop. Counter: ", loopCounter);
   console.log("questions array: ", questionsArray);
+  const numberOfQuestions = this.questionsArray.length;
+
 //     this.currentQuestion = question;
      let waitingForAnswer = false;
 //
@@ -63,29 +37,33 @@ QuizLooper.prototype.questionLoop = function (currentQuestion, loopCounter, ques
        if (waitingForAnswer == false) {
          PubSub.publish('QuizLooper:question-ready', currentQuestion)
          waitingForAnswer = true;
-//     //  while (waitingForAnswer) { // logic works but crashes / hangs browser
            console.log('waiting for answer: ', waitingForAnswer);
-//
            PubSub.subscribe('QuizView:answer-selected', (questionAndAnswer) => {
-             console.log('got answer to check: ', questionAndAnswer.detail);
-            const answer = questionAndAnswer.detail[0];
-            const question = questionAndAnswer.detail[1];
-            this.answerResult = this.checkAnswer(question, answer);
-            if (this.answerResult[0] == false) {
-              this.addScoreToTotal(this.answerResult[1]);
-            }; // end if
-            PubSub.publish('QuizLooper:answer-checked', this.answerResult);
-            waitingForAnswer = false;
-            console.log('waiting for answer: ', waitingForAnswer);
-//     // this waitingForAnswer loop seems to be working but I'm not convinced it should!
-//     // ha - it 'worked' because the category only had 1 question in it! Back to the while loop
-//     // while loop won't work as it ties up the program and prevents the user actually getting the question
+            const answerResult = this.processAnswer(questionAndAnswer.detail);
+            PubSub.publish('QuizLooper:answer-checked', answerResult);
+            loopCounter++;
+              if (loopCounter < numberOfQuestions) {
+                currentQuestion = questionsArray[loopCounter];
+                this.questionLoop(currentQuestion, loopCounter, questionsArray);
+              } else {
+                console.log("LAst question answered: number ", loopCounter);
+                PubSub.publish('QuizLooper:last-question-answered', loopCounter);
+                return loopCounter;
+              };
              }); // end subscribe
-//     //      }; // end while
-//
          }; // end if
-//     //  }; // end for
  };
+
+QuizLooper.prototype.processAnswer = function (questionAndAnswerData) {
+  console.log('got answer to check: ', questionAndAnswerData);
+ const answer = questionAndAnswerData[0];
+ const question = questionAndAnswerData[1];
+ this.answerResult = this.checkAnswer(question, answer);
+ if (this.answerResult[0] == false) {
+   this.addScoreToTotal(this.answerResult[1]);
+ }; // end if
+ return this.answerResult;
+};
 
 QuizLooper.prototype.addScoreToTotal = function (score) {
   this.runningTotal += score;
@@ -97,8 +75,6 @@ QuizLooper.prototype.checkAnswer = function (question, answer) {
   let result = [];
   let playersAnswer = [];
   const currentQuestion = question;
-  console.log('answer is: ', answer);
-  console.log('question is: ', question);
 //  const playersAnswer = answer.value;
 // hardcode into an array until one can be sent
   playersAnswer.push(answer);
@@ -111,7 +87,6 @@ QuizLooper.prototype.checkAnswer = function (question, answer) {
   }; // end while
   rightness = playersAnswer.length;
   result = [Boolean(wrongness), rightness];
-  console.log('result: ', result);
   return result;
 }; // end checkAnswer
 
