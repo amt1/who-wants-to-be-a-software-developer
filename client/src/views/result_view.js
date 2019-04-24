c58a9b1125781be9757f35b9607e9d44df2675ea
Barker <benbarks.v1@gmail.com>
2019-04-24 23:09:26 +0100

    result_view aligned with css base for divs

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -2,9 +2,8 @@ const PubSub = require('../helpers/pub_sub.js');
 const QuizView = require('./quiz_view.js');
 
 
-const ResultView = function (container, answerContainer) {
+const ResultView = function (container) {
   this.container = container;
-  this.answerContainer = answerContainer;
 };
 
 ResultView.prototype.bindEvents = function () {
@@ -23,23 +22,45 @@ ResultView.prototype.bindEvents = function () {
 
 };
 
+// PubSub.subscribe('QuizLooper:answer-checked', (evt) => {
+//   if (evt.detail == true) {
+//     const result = "Correct!"
+//   }
+//   else {
+//     const result = "Incorrect!"
+//   };
+//   // const view = this.displayResult(evt.detail[0]);
+//   this.renderResult(result)
+// });
+// };
+
+// ResultView.prototype.renderResult = function(result){
+// const resultBox = document.createElement('div')
+// resultBox.id = 'resultBox';
+//
+// const resultHead = document.createElement('h1');
+// resultHead.textContent = result;
+// resultHead.classList.add('quiz-name');
+// resultBox.appendChild(resultHead);
+//
+// this.container.appendChild(resultBox);
+// }
+
 ResultView.prototype.emptyElement = function () {
   this.container.innerHTML = '';
 };
 ResultView.prototype.displayResult = function(result) {
-  const quizContainer = document.createElement('div');
-  quizContainer.class = 'result-view';
+  const resultBox = document.createElement('div')
+  // resultBox.id = 'resultDiv';
+  resultBox.classList.add('quiz-wrapper')
 
-  const answer = document.createElement('p', result);
-  answer.textContent = `Your answer was: ${result}`
-  quizContainer.appendChild(answer);
+  const resultHead = document.createElement('p');
+  resultHead.textContent = result;
+  resultHead.classList.add('questions');
+  resultBox.appendChild(resultHead);
 
-  // const resultText = document.createElement('p');
-  // resultText.textContent = result;
-  // quizContainer.appendChild(resultText);
-
-  this.container.appendChild(quizContainer);
-  this.createNextButton(quizContainer);
+  this.container.appendChild(resultBox);
+  this.createNextButton(resultBox);
 };
 
 ResultView.prototype.createNextButton = function (quizContainer) {
@@ -54,46 +75,46 @@ ResultView.prototype.createNextButton = function (quizContainer) {
     console.log("next button clicked");
   });
 };
-
-ResultView.prototype.populateAnswer = function (playerAnswer, question) {
-  const answerContainer = document.createElement('div');
-  answerContainer.class = 'answer-result';
-
-  const questionView = this.createHeading('The question was:');
-  answerContainer.appendChild(questionView);
-
-  const playerQuestion = document.createElement('p');
-  playerQuestion.textContent = question.question;
-  answerContainer.appendChild(playerQuestion);
-
-  const label = document.createElement('p', question.correct_answer.slice(2, -2) );
-  label.textContent= `The correct answer is: ${question.correct_answer.slice(2, -2)}`;
-  answerContainer.appendChild(label);
-
-  const info = document.createElement('p');
-  // workaround the storedvalue in db until permanet fix
-  info.textContent = question.correct_answer.slice(2, -2);
-  answerContainer.appendChild(info);
-
-
-  const moreInfo = document.createElement('p', question.link);
-  moreInfo.textContent = `You can read more: ${question.link}`;
-  answerContainer.appendChild(moreInfo);
-  this.container.appendChild(answerContainer);
-
-}
-
-ResultView.prototype.createHeading = function (textContent) {
-  const heading = document.createElement('h3');
-  heading.textContent = textContent;
-  return heading;
-};
 //
-ResultView.prototype.createDetail = function (text, label) {
-  const detail = document.createElement('p');
-  detail.textContent = `${text}: ${label}`;
-  console.log(detail.textContent);
-};
+// ResultView.prototype.populateAnswer = function (playerAnswer, question) {
+//   const answerContainer = document.createElement('div');
+//   answerContainer.class = 'answer-result';
+//
+//   const questionView = this.createHeading('The question was:');
+//   answerContainer.appendChild(questionView);
+//
+//   const playerQuestion = document.createElement('p');
+//   playerQuestion.textContent = question.question;
+//   answerContainer.appendChild(playerQuestion);
+//
+//   const label = document.createElement('p', question.correct_answer.slice(2, -2) );
+//   label.textContent= `The correct answer is: ${question.correct_answer.slice(2, -2)}`;
+//   answerContainer.appendChild(label);
+//
+//   const info = document.createElement('p');
+//   // workaround the storedvalue in db until permanet fix
+//   info.textContent = question.correct_answer.slice(2, -2);
+//   answerContainer.appendChild(info);
+//
+//
+//   const moreInfo = document.createElement('p', question.link);
+//   moreInfo.textContent = `You can read more: ${question.link}`;
+//   answerContainer.appendChild(moreInfo);
+//   this.container.appendChild(answerContainer);
+//
+// }
+//
+// ResultView.prototype.createHeading = function (textContent) {
+//   const heading = document.createElement('h3');
+//   heading.textContent = textContent;
+//   return heading;
+// };
+// //
+// ResultView.prototype.createDetail = function (text, label) {
+//   const detail = document.createElement('p');
+//   detail.textContent = `${text}: ${label}`;
+//   console.log(detail.textContent);
+// };
 
 
 
